bcab17adddd27a45cc24c31e92f80ec9bb6047a1
nachtriebyann-prog <nachtriebyann@gmail.com>
2026-05-12 18:52:18 -0400

    chore: remove debug-env endpoint

--- api/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const vars = ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_PHONE_NUMBER', 'SUPABASE_URL', 'SUPABASE_KEY'];
-  const result = {};
-  for (const v of vars) {
-    const val = process.env[v];
-    result[v] = val ? `SET (${val.length} chars, starts: ${val.slice(0, 6)}...)` : 'MISSING';
-  }
-  return res.status(200).json(result);
-}
